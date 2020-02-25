26a14cd7317cdf5ae5c9c2b4ae6b02e92bb6c41f
JuanpaHdez <a01423444@itesm.mx>
2020-02-25 12:09:37 -0600

    test1

--- angular-mysql-crud/server/src/keys.ts
+++ angular-mysql-crud/server/src/keys.ts
@@ -2,7 +2,7 @@ export default {
     database:{
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: '', //root//
         database: 'ng_cuestionarios_db'
     }
 }
