94eddcd4f14dc901c91510184482d386c9a22ac8
unknown <a01421467@itesm.mx>
2020-03-05 20:25:38 -0600

    started miProfile display and edit

--- angular-mysql-crud/client/src/app/components/my-profile/my-profile.component.ts
+++ angular-mysql-crud/client/src/app/components/my-profile/my-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-my-profile',
@@ -6,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.css']
 })
 export class MyProfileComponent implements OnInit {
-
-  constructor() { }
   us: JSON;
+  editando: boolean;
+  nacimiento: Date;
+  constructor(public datepipe: DatePipe){
+    this.editando = false;
+  }
+
   ngOnInit(): void {
     this.us = JSON.parse(localStorage.getItem('usuario'));
-    console.log('el usuario es'  + this.us['usuario'] );
-    console.log(localStorage.getItem('usuario'));
+    //ESTO NO ME ESTA DANDO LA FECHA CORRECTA AIUDA!!!
+    this.nacimiento = new Date (this.datepipe.transform(this.us['fechaNacimiento'], 'yyyy/MM/dd'));
+    console.log('a   '+this.nacimiento);
+  }
+
+  editar( editando: boolean) {
+    this.editando = editando;
+  }
+
+  saveChanges(){
+    this.editar(false);
   }
 
 }
