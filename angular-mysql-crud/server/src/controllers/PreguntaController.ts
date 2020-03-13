0c3cdb5b842d74cca338039a6e915593e7a5f7a8
Omar Quintero <omar.quintero.ms@gmail.com>
2020-03-12 21:56:56 -0600

    Catalogos crear y actualizar

--- angular-mysql-crud/server/src/controllers/PreguntaController.ts
+++ angular-mysql-crud/server/src/controllers/PreguntaController.ts
@@ -1,17 +1,33 @@
 import { Request, Response} from 'express';
-import { PreguntaService } from '../services';
-import { Pregunta } from '../models';
+import { PreguntaService, CatalogoPreguntaService, TipoPreguntaService } from '../services';
+import { Pregunta, CatalogoPregunta, TipoPregunta, PreguntaRequest } from '../models';
 
 export class PreguntaController {
     
     static async listarPreguntas(req: Request, res: Response) {
         const { idCuestionario, idCatalogoPregunta} = req.query;
         const _pregunta: Pregunta[] = await PreguntaService.listarPreguntas(idCuestionario,idCatalogoPregunta);
-        res.json(_pregunta);
+        const preguntaFormat = _pregunta.map((item: any) => new Pregunta(item))
+        for (const pregunta of preguntaFormat) {
+            pregunta.catalogoPregunta = new CatalogoPregunta(await CatalogoPreguntaService.obtenerCatalogoPregunta(pregunta.idCatalogoPregunta!)); 
+            pregunta.tipoPregunta = new TipoPregunta(await TipoPreguntaService.obtenerTipoPregunta(pregunta.idTipoPregunta!));            
+        }
+        res.json(preguntaFormat);
     }
 
     static async crearPregunta(req: Request, res: Response) {
-        let pregunta: Pregunta = req.body;    
+        let preguntaR: PreguntaRequest = req.body;    
+        let pregunta = new Pregunta(preguntaR);
+        let catalogos = await CatalogoPreguntaService.listarCatalogoPreguntas(preguntaR.pregunta);
+        if (catalogos.length > 0) { // La nueva pregunta ya existe
+            pregunta.idCatalogoPregunta = catalogos[0].idCatalogoPregunta
+        } else {                    // La nueva pregunta no existe
+            let newCatalogo = new CatalogoPregunta({pregunta: preguntaR.pregunta})
+            await CatalogoPreguntaService.crearCatalogoPregunta(newCatalogo);
+            catalogos = await CatalogoPreguntaService.listarCatalogoPreguntas(preguntaR.pregunta);
+            pregunta.idCatalogoPregunta = catalogos[0].idCatalogoPregunta
+        }
+
         await PreguntaService.crearPregunta(pregunta);     
         res.json({'message':'saved pregunta'});
     }
@@ -19,16 +35,45 @@ export class PreguntaController {
     static async obtenerPregunta(req: Request, res: Response) {
         const { idPregunta} = req.params;
         const _pregunta: Pregunta = await PreguntaService.obtenerPregunta(parseInt(idPregunta));
-        if(_pregunta == null){
+        const preguntaFormat = new Pregunta(_pregunta);
+        preguntaFormat.catalogoPregunta = new CatalogoPregunta(await CatalogoPreguntaService.obtenerCatalogoPregunta(preguntaFormat.idCatalogoPregunta!));            
+        preguntaFormat.tipoPregunta = new TipoPregunta(await TipoPreguntaService.obtenerTipoPregunta(preguntaFormat.idTipoPregunta!));            
+
+        if(preguntaFormat == null){
             res.json({message:'the pregunta is not valid'});
         }else{
-            res.json(_pregunta);
+            res.json(preguntaFormat);
         }
     }
 
     static async actualizarPregunta(req: Request, res: Response) {
         const { idPregunta } = req.params;
-        let pregunta: Pregunta = req.body;    
+        let preguntaR: PreguntaRequest = req.body;    
+        let pregunta = new Pregunta(preguntaR);
+        let preguntaAnterior = await PreguntaService.obtenerPregunta(parseInt(idPregunta));
+        let preguntasMismoCatalogo = await PreguntaService.listarPreguntas(preguntaAnterior.idCatalogoPregunta);
+        let catalogos = await CatalogoPreguntaService.listarCatalogoPreguntas(preguntaR.pregunta); 
+        if (preguntasMismoCatalogo.length <= 1 && catalogos.length > 0) { //La opcion anterior solo yo la uso y la nueva ya existe
+            pregunta.idCatalogoPregunta = catalogos[0].idCatalogoPregunta;
+            await PreguntaService.actualizarPregunta(parseInt(idPregunta), pregunta);    
+            await CatalogoPreguntaService.eliminarCatalogoPregunta(preguntaAnterior.idCatalogoPregunta!);
+
+        }else if (preguntasMismoCatalogo.length <= 1 && catalogos.length <= 0) { // La opcion anterior solo yo la uso, y la nueva no existe
+            await CatalogoPreguntaService.actualizarCatalogoPregunta(preguntaAnterior.idCatalogoPregunta!, { pregunta: preguntaR.pregunta });
+            await PreguntaService.actualizarPregunta(parseInt(idPregunta), pregunta);
+
+        } else if (preguntasMismoCatalogo.length > 1 && catalogos.length > 0) { //La opcion anterior mas personas la usan y la nueva ya existe
+            pregunta.idCatalogoPregunta = catalogos[0].idCatalogoPregunta;
+            await PreguntaService.actualizarPregunta(parseInt(idPregunta), pregunta);    
+
+        }else if (preguntasMismoCatalogo.length > 1 && catalogos.length <= 0) { // La opcion anterior mas personas la usan, y la nueva no existe
+            await CatalogoPreguntaService.crearCatalogoPregunta({ pregunta: preguntaR.pregunta });
+            let newCatalogos = await CatalogoPreguntaService.listarCatalogoPreguntas(preguntaR.pregunta);
+            pregunta.idCatalogoPregunta = newCatalogos[0].idCatalogoPregunta;
+            await PreguntaService.actualizarPregunta(parseInt(idPregunta), pregunta);
+
+        }
+
         await PreguntaService.actualizarPregunta(parseInt(idPregunta), pregunta);    
         res.json({'message':'the pregunta was updated '})
     }
