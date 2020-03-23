ef4e5afebbd062cc1a5fd9c7238874fe1153d835
unknown <a01421467@itesm.mx>
2020-03-23 13:57:57 -0600

    editar y eliminar pregunta

--- angular-mysql-crud/client/src/app/components/custionario-form/custionario-form.component.ts
+++ angular-mysql-crud/client/src/app/components/custionario-form/custionario-form.component.ts
@@ -3,6 +3,7 @@ import {Cuestionario, PreguntaRequest, Opcion, OpcionRequest,Pregunta, _TipoPreg
 import { CuestionarioService, PreguntaService, OpcionService } from '../../services/index';
 import {provideRoutes} from '@angular/router';
 import { Router, ActivatedRoute } from '@angular/router';
+import {newArray} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'app-custionario-form',
@@ -15,14 +16,20 @@ export class CustionarioFormComponent implements OnInit {
   @ViewChild('closeModal') private closeModal: ElementRef;
   @ViewChild('closeModalOpcion') private closeModalOpcion: ElementRef;
   @ViewChild('closeModalOpcionEditar') private closeModalOpcionEditar: ElementRef;
+  @ViewChild('closeModalPreguntaEditar') private closeModalPreguntaEditar: ElementRef;
 
   cuestionario?: Cuestionario = new Cuestionario();
 
   listTipos = _TipoPregunta.listTipos;
+
   pregunta?: PreguntaRequest = new PreguntaRequest();
   opcion?: OpcionRequest = new OpcionRequest();
 
   laOpcionString?: string;
+  laPreguntaString?: string;
+
+  eliminandoPregunta?: boolean = false;
+
   constructor(private cuestionariosServices: CuestionarioService, private preguntaService: PreguntaService,
               private opcionService: OpcionService, private router: Router, private activatedRoute: ActivatedRoute) {
     this.cuestionario.idCuestionario = +this.activatedRoute.snapshot.paramMap.get('idCuestionario');
@@ -41,39 +48,54 @@ export class CustionarioFormComponent implements OnInit {
         this.cuestionario = resCuestionario;
 
         // Obtener todas las preguntas
-        this.preguntaService.listarPreguntas(this.cuestionario.idCuestionario)
-          .subscribe( resPreguntas => {
-            this.cuestionario.preguntas = resPreguntas.map((item: Pregunta) => new Pregunta(item));
-
-            //Obtener todas las opciones
-            this.listarOpciones();
-          }, err => {
-            console.log(err);
-          });
+        this.listarPreguntas(true);
       }, err => {
         console.log(err);
       });
   }
 
-  listarOpciones() {
+  listarPreguntas(listarOpciones: boolean) {
+    this.preguntaService.listarPreguntas(this.cuestionario.idCuestionario)
+      .subscribe( resPreguntas => {
+        this.cuestionario.preguntas = resPreguntas.map((item: Pregunta) => new Pregunta(item));
+
+        //Obtener todas las opciones
+        if (listarOpciones == true ) {
+          this.listarOpciones();
+        }
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  listarOpciones(pregunta?: Pregunta) {
     //Obtener todas las opciones
-    for (const pregunta of this.cuestionario.preguntas) {
-      if (pregunta.tipoPregunta.tipo !== _TipoPregunta.TipoPreguntaEnum.ABIERTA) {
-        this.opcionService.listarOpcions(null, pregunta.idPregunta).subscribe(opciones => {
-          pregunta.opciones = opciones.map((item: Opcion) => new Opcion(item));
-        });
-      }
+    let preguntas: Pregunta[] ;
+    if (pregunta == null) {
+      preguntas = this.cuestionario.preguntas;
+    }else{
+      preguntas = newArray<Pregunta>(1);
+      preguntas[0] = pregunta;
     }
+      for (const pregunta of preguntas) {
+        if (pregunta.tipoPregunta.tipo !== _TipoPregunta.TipoPreguntaEnum.ABIERTA) {
+          this.opcionService.listarOpcions(null, pregunta.idPregunta).subscribe(opciones => {
+            pregunta.opciones = opciones.map((item: Opcion) => new Opcion(item));
+          });
+        }
+      }
+
   }
 
   ///////////////////////////   ASIGNACIONES ////////////////////////////////////
-  asignarPregunta(idPregunta: number) {
-    this.opcion.idPregunta = idPregunta;
-    console.log("Agregando una opcion a la pregunta " +   this.opcion.idPregunta);
+  asignarPregunta(pregunta: PreguntaRequest) {
+    this.pregunta = pregunta;
+    this.opcion.idPregunta = pregunta.idPregunta;
+    this.laPreguntaString = this.pregunta['catalogoPregunta'].pregunta;
   }
   asignarOpcion( opcion: OpcionRequest){
-    this.opcion=opcion;
-    this.laOpcionString =this.opcion['catalogoOpcion'].opcion;
+    this.opcion = opcion;
+    this.laOpcionString = this.opcion['catalogoOpcion'].opcion;
   }
 
   ///////////////////////////   PREGUNTA ////////////////////////////////////
@@ -83,6 +105,7 @@ export class CustionarioFormComponent implements OnInit {
       .subscribe( res => {
         console.log(res);
         this.closeModal.nativeElement.click();
+
         this.pregunta.pregunta = '';
         this.cargarCuestionario();
       }, err => {
@@ -91,13 +114,39 @@ export class CustionarioFormComponent implements OnInit {
   }
 
   eliminarPregunta(idPregunta: number) {
+    console.log('eliminar pregunta');
+    this.eliminandoPregunta = true;
+
       this.preguntaService.eliminarPregunta(idPregunta)
         .subscribe( res => {
           console.log(res);
-          this.cargarCuestionario();
+          this.eliminandoPregunta = false;
+          this.listarPreguntas(false);
         }, err => {
           console.log(err);
         });
+  }
+
+  editarPregunta() {
+    if (this.eliminandoPregunta == true ) return;
+    console.log('editar pregunta');
+    this.pregunta.pregunta = this.laPreguntaString;
+    console.log(this.pregunta);
+    this.preguntaService.actualizarPregunta(this.pregunta.idPregunta, this.pregunta)
+      .subscribe( res => {
+        console.log(res);
+        this.cargarCuestionario();
+        this.closeModalPreguntaEditar.nativeElement.click();
+        this.laPreguntaString = '';
+      }, err => {
+        console.log(err);
+      });
+  }
+
+  AbrirModalEditarPregunta() {
+    if (this.eliminandoPregunta == true ) return;
+    // abro modal
+    document.getElementById("openModalButton").click();
   }
   ///////////////////////////   OPCIONES ////////////////////////////////////
   agregarOpcion(){
@@ -113,13 +162,13 @@ export class CustionarioFormComponent implements OnInit {
         });
   }
 
-  eliminarOpcion(idOpcion: number) {
+  eliminarOpcion(idOpcion: number, pregunta: Pregunta) {
       console.log('eliminando la opcion' + idOpcion)
       this.opcionService.eliminarOpcion(idOpcion)
         .subscribe( res => {
           console.log(res);
-          //Obtener todas las opciones
-          this.listarOpciones();
+          //Volver a listar las opciones de esta pregunta
+          this.listarOpciones(pregunta);
         }, err => {
           console.log(err);
           console.log('err');
