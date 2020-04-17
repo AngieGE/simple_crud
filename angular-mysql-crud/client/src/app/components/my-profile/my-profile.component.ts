798f2b685e193ee774e4186493112ec99afa5bae
unknown <a01421467@itesm.mx>
2020-04-17 12:46:08 -0500

    gui for myProfile

--- angular-mysql-crud/client/src/app/components/my-profile/my-profile.component.ts
+++ angular-mysql-crud/client/src/app/components/my-profile/my-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { Usuario} from '../../models/index';
-import {ManagerService, UsuarioService} from '../../services';
+import {ManagerService, UsuarioService, AplicacionService, CuestionarioService} from '../../services';
 
 
 @Component({
@@ -13,14 +13,36 @@ import {ManagerService, UsuarioService} from '../../services';
 export class MyProfileComponent implements OnInit {
   editando: boolean;
   usuario: Usuario;
+  cuestionariosActivos: number = 0;
+  //cuestionariosEliminados: number = 0;
+  respuestasRecibidas: number = 0;
+  respuestasEnviadas: number = 0;
 
-  constructor(public datepipe: DatePipe, private manager: ManagerService, private usuarioService: UsuarioService) {
+
+
+  constructor(public datepipe: DatePipe, private manager: ManagerService, private usuarioService: UsuarioService,
+              private aplicacionService: AplicacionService, private cuestionarioService: CuestionarioService) {
     this.editando = false;
   }
 
   ngOnInit(): void {
    this.usuario = new Usuario(this.manager.usuario);
    this.usuario.localFechaNacimiento = this.datepipe.transform(this.usuario.fechaNacimiento, 'yyyy-MM-dd');
+    
+   this.aplicacionService.listarAplicaciones(this.manager.usuario.idUsuario, null)
+        .subscribe(res =>{ console.log(res.length); this.respuestasEnviadas = res.length;});
+    
+    this.cuestionarioService.listarCuestionarios(this.manager.usuario.idUsuario, null)
+        .subscribe(cuestionarios => {  
+          this.cuestionariosActivos = cuestionarios.length
+          cuestionarios.forEach(element => {
+            console.log(element.idCuestionario);
+            this.aplicacionService.listarAplicaciones(null, element.idCuestionario)
+                .subscribe(res => {this.respuestasRecibidas += res.length;});
+          });
+        },err => {console.log(err);});
+
+    
   }
 
   editar( editando: boolean) {
