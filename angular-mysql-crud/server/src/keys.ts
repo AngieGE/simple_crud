aced744e8d519f900dc0311fc7195e7e8c265f54
unknown <a01421467@itesm.mx>
2020-05-21 23:56:19 -0500

    delete keys file

--- angular-mysql-crud/server/src/keys.ts
+++
@@ -1,8 +0,0 @@
-export const database = {
-        server: '35.235.89.179',
-        user: 'SA',
-        password: 'aA01421467.',
-        database: 'cuestionariosDB'
-    }
-
-export const apiPort = 3000;
